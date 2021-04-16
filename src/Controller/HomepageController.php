d3479543bc9851932ab660c1d6d6605a1bd93311
Lukas Patackas <xxlas@users.noreply.github.com>
2021-04-16 20:03:34 +0300

    Basic gameplay logic implemented with a few exceptions.

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,19 +2,58 @@
 
 namespace App\Controller;
 
+use App\Entity\Board;
+use App\Entity\BoardState;
+use App\Services\BoardLogicService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
     /**
+     * @var BoardLogicService
+     */
+    private $boardLogicService;
+    /**
+     * @var FlashBagInterface
+     */
+    private $flashBag;
+
+    /**
+     * HomepageController constructor.
+     */
+    public function __construct(BoardLogicService $boardLogicService, FlashBagInterface $flashBag)
+    {
+        $this->boardLogicService = $boardLogicService;
+        $this->flashBag = $flashBag;
+    }
+
+
+    /**
      * @Route("/homepage", name="homepage")
      */
-    public function index(): Response
+    public function index(Request $req): Response
     {
+        $requestFields = $req->query->all();
+        $message = null;
+        if(sizeof($requestFields) > 0) {
+            $board = new Board($requestFields['size'], $requestFields['turn']);
+            $boardState = new BoardState($board, $requestFields['state']);
+            $message = $this->boardLogicService->calculateTurn($board, $boardState, $requestFields['selectedPos']);
+            $this->flashBag->add('warning', $message);
+        } else {
+            $board = new Board(3);
+            $boardState = new BoardState($board);
+        }
+
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
+            'board' => $board,
+            'state' => $boardState,
+            'turn' => $board->getTurn(),
         ]);
     }
 }
